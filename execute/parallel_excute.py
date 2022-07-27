cf4a03c0e2a3f6d8a89f06d137276f2c6c610bd3
directornie <directornie@xiaoe-tech.com>
2022-07-27 19:15:38 +0800

    解决使用shell命令运行项目出现ModuleNotFoundError报错V3

--- execute/parallel_excute.py
+++ execute/parallel_excute.py
@@ -4,7 +4,9 @@ import os
 import sys
 import unittest
 
-sys.append(sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(os.path.split(rootPath)[0])
 
 from tools.GenerateHtmlReport import GenerateHtmlReport
 from tools.HTMLTestRunner import HTMLTestRunner
